05f625def56608b7e0788e4bba21d6a8e0d44338
Rootjang92 <janggeunho7@gmail.com>
2024-03-04 19:58:10 +0900

    pure recursion example

---
+++ 재귀/pureRecursion.js
@@ -0,0 +1,12 @@
+function collectOddvalues(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) return newArr;
+
+  if (arr[0] % 2 !== 0) newArr.push(arr[0]);
+
+  newArr = newArr.concat(collectOddvalues(arr.slice(1)));
+  return newArr;
+}
+
+console.log(collectOddvalues([1,2,3,4,5,6,7,8,9]));
