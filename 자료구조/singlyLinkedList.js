2fe58a32ba61f1b205f348dc259ba6a72790ab20
Rootjang92 <janggeunho7@gmail.com>
2024-03-11 21:03:23 +0900

    단일 연결 리스트트 push, pop, shift, unshift, get, set

---
+++ 자료구조/singlyLinkedList.js
@@ -0,0 +1,154 @@
+// 데이터 저장 자료구조
+// 순서에 따라 데이터 저장
+// 배열과 다른 점은 다음 데이터를 가리키는 인덱스 없이 다수의 데이터 요소로 구성된다.
+// pointer로 연결되어 사용된다. (기차 처럼)
+// element = node
+// 다음 노드가 없을 경우 null을 저장한다.
+// head - 시작 노드, tail - 마지막 노드
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// const first = new Node("Hi");
+// first.next = new Node("therer");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+
+// console.log(first);
+
+class SinglyLinkedList extends Node {
+  constructor() {
+    super();
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // 주어진 값으로 새로운 노드를 생성.
+  // 길이가 0일 경우 그 값은 head이자 tail
+  // 리스트가 비어있지 않다면, 마지막 노드의 next를 새롭게 생성된 노드를 가리키도록 한다.
+  // 길이 증가.
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+
+  // 리스트 맨 마지막 노드 제거
+  // 리스트가 비면 undefined 반환
+  // tail값 업데이트
+  // 길이 빼기
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head, newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+
+  // 리스트 맨 앞 제거
+  // 헤드 제거 후 next 값을 헤드로 지정하기
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
+
+  // 새로운 노드를 맨 앞에 넣는 것.
+  // 헤드와 테일이 모두 변경되어야 함.
+  // 길이 1증가, 마지막 list 반영
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    
+    this.length++;
+    return this;
+  }
+
+  // 주어진 위치의 노드를 반환하는 함수
+  // index는 양수이거나 길이보다 같거나 작아야 함.
+  // 반복문을 통해 인덱스에 해당하는 노드를 반환
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+
+    console.log(current, counter);
+    return current;
+  }
+
+  // index의 값을 val로 update
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+}
+
+const list = new SinglyLinkedList();
+
+list.push("Hello");
+list.push("Good bye");
+list.push("!");
+list.get(2);
+list.set(2, "?");
+list.set(0, "Bye");
+
+console.log(list);
+
